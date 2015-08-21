6c11baab4a237c927d5bdca638941e72e73e6657
sndrs <a@snd.rs>
2015-08-21 13:50:58 +0100

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,29 @@
+import gulp from 'gulp';
+import watch from 'gulp-watch';
+
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+
+import bs from 'browser-sync';
+
+const browserSync = bs.create();
+
+gulp.task('default', ['sass'], () => {
+    browserSync.init({
+        "server": true,
+        "port": 3000,
+        "minify": false
+    });
+    gulp.watch('style.scss', ['sass']);
+    gulp.watch("index.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', () =>
+    gulp.src('style.scss')
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('.'))
+        .pipe(browserSync.stream())
+)
